6e41f5f0d5bc14948e4176ef49b7f1b8526972b0
vinod supnekar <vinod.supnekar@gmail.com>
2023-02-08 22:46:14 +0530

    struct let vs class let variable

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -45,3 +45,26 @@ case let capital?:
 }
 
 // value is London
+
+
+public class ThermometerClass {
+  private(set) var temperature: Double = 0.0
+  public func registerTemperature(_ temperature: Double) {
+    self.temperature = temperature
+  }
+}
+
+let thermometerClass = ThermometerClass()
+thermometerClass.registerTemperature(56.0)
+
+public struct ThermometerStruct {
+    private(set) var temperature: Double = 0.0
+  public mutating func registerTemperature(_ temperature: Double) {
+    self.temperature = temperature
+  }
+}
+
+//let thermometerStruct = ThermometerStruct()
+//thermometerStruct.registerTemperature(55)
+// Above let thermometerStruct gives compiler error as it cannot be modified as class let variable.
+//To fix this make thermometerStruct as var
