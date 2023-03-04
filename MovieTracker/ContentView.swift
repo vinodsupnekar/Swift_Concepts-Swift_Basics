62c6d7ca2ca0a9b92c5a40e4463f2a4385dd7e0d
vinod supnekar <vinod.supnekar@gmail.com>
2023-03-04 15:52:42 +0530

    List with Sections and controls like Textfield Toggle and Slider

--- MovieTracker/ContentView.swift
+++ MovieTracker/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var title = ""
+    @State var rating = 3.0
+    @State var seen = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            Section{
+       TextField("Movie Title", text: $title)
+            }
+            Section {
+                HStack {
+                    Spacer()
+                    Text(String(repeating: "*", count: Int(rating))).foregroundColor(.yellow).font(.title)
+                Spacer()
+                }
+                Slider(value: $rating, in: 1...5, step: 1)
+
+            }
+            Section {
+                Toggle(isOn: $seen){
+                    if title == "" {
+                        Text("Has seen the movie")
+                    } else {
+                        Text("Has seen the  \(title)")
+
+                    }
+                }
+            }
+        }.listStyle(GroupedListStyle())
     }
 }
 
