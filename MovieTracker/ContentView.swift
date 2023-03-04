2b9a5d057c79393861fc5cf487e08ba2c20c06a6
vinod supnekar <vinod.supnekar@gmail.com>
2023-03-04 14:21:01 +0530

    Initial Commit

---
+++ MovieTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieTracker
+//
+//  Created by Rjvi on 04/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieTracker/MovieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTrackerApp.swift
+//  MovieTracker
+//
+//  Created by Rjvi on 04/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
